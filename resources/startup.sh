9d2e4848698bb8ec730ac1a89106b99e70335e02
Joshua Sprey <joshua.sprey@cloudogu.com>
2022-08-02 16:47:34 +0200

    #1 Add default dogu configuration

--- resources/startup.sh
+++ resources/startup.sh
@@ -19,7 +19,18 @@ echo "                       'V/(/////////////////////////////V'      "
 # Menu.json gets mounted from a configmap so other files in /var/www/html/warp would be deleted.
 sed -i "s|/warp/menu.json|/warp/menu/menu.json|g" /var/www/html/warp/warp.js
 
-#sleep infinity
+# include default_dogu in default-dogu.conf
+echo "[nginx] configure default redirect ..."
+doguctl template /etc/nginx/include.d/default-dogu.conf.tpl /etc/nginx/include.d/default-dogu.conf
+
+## configure the access to static html content
+#echo "[nginx] configure custom content pages ..."
+#doguctl template /etc/nginx/include.d/customhtml.conf.tpl /etc/nginx/include.d/customhtml.conf
+#
+## render main configuration to include log_level
+#echo "[nginx] configure logging ..."
+#export_log_level
+#doguctl template /etc/nginx/nginx.conf.tpl /etc/nginx/nginx.conf
 
 # Start nginx
 echo "[nginx-static] starting nginx service..."
